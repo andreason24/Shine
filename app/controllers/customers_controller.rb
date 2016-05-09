a35de30c9d69c3b59954c559e1111b8141ca3fad
and_reason <andreason24@gmail.com>
2016-05-09 12:44:08 +0300

    angular search, ajax request

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,14 +6,18 @@ class CustomersController < ApplicationController
     if params[:keywords].present?
       @keywords = params[:keywords]
       customer_search_term = CustomerSearchTerm.new(@keywords)
-      # binding.pry
       @customers = Customer.where(
           customer_search_term.where_clause,
           customer_search_term.where_args).
           order(customer_search_term.order).
           offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
-      @customers = Customer.first 10
+      @customers = []
+    end
+
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @customers }
     end
   end
 end
